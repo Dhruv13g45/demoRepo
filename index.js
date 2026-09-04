b58ae51c580b53237825d3f81bf2477b713d3025
Dhruv Goradia <dhruvhgoradia@gmail.com>
2026-09-04 16:03:45 +0530

    fix: add missing semicolons and ensure consistent formatting in index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-console.log("This is the main branch content")
+console.log("This is the main branch content");
 
-console.log("This is the modified content")
+console.log("This is the modified content");
 
+console.log("This is the new change");
 
-console.log("This is the new change")
+console.log("Test branch for pr funnel");
